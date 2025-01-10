cb93713400cea1b801481bd8a62c7da7e0c6bd60
Swift <86579464+swiftscripts0@users.noreply.github.com>
2025-01-11 02:38:52 +1030

    Update app.js

--- app.js
+++ app.js
@@ -6,9 +6,6 @@ app.get("/", (req, res) => res.type('html').send(html));
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
-
 const html = `
 <!DOCTYPE html>
 <html>
